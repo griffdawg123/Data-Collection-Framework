ac5b6f160807b6692e89ed67732dbf7b894dccb3
griffdawg123 <doylegriffin32@gmail.com>
2024-03-11 17:12:42 +1100

    can connect to thingy and display data

--- src/ble/ble_generators.py
+++ src/ble/ble_generators.py
@@ -6,7 +6,7 @@ import numpy as np
 from PyQt6.QtCore import pyqtSlot
 from qasync import QEventLoop, QThreadExecutor, asyncClose
 
-class ReadThread(DataThread):
+class BLEThread(DataThread):
     def __init__(self, device_address: str, service_uuid) -> None:
         super().__init__()
         self.uuid = service_uuid
@@ -14,7 +14,27 @@ class ReadThread(DataThread):
         self.client = BleakClient(self.device_address, lambda client: print(f"Disconnected from client: {client.address}"))
         self.loop = asyncio.get_event_loop()
         self.connect_task = self.loop.create_task(self.connect())
-        self.current_val = bytes(0)
+        self.current_val = b'\x00'
+
+    async def connect(self):
+        await self.client.connect()
+
+    def cleanup(self):
+        print("cleaning up")
+        _ = self.disconnect()
+
+    @asyncClose
+    async def disconnect(self):
+        print("disconnecting")
+        await self.client.disconnect()
+        print("disconnected successfully")
+
+    def set_current_val(self, data: bytes):
+        self.current_val = data
+
+class ReadThread(BLEThread):
+    def __init__(self, device_address: str, service_uuid) -> None:
+        super().__init__(device_address, service_uuid)
 
     # updates current value and then emits it
     def get_value(self):
@@ -30,21 +50,25 @@ class ReadThread(DataThread):
     async def read_value(self):
         self.current_val = await self.client.read_gatt_char(self.uuid)
 
-    async def connect(self):
-        await self.client.connect()
+class NotifyThread(BLEThread):
+    def __init__(self, device_address: str, service_uuid) -> None:
+        super().__init__(device_address, service_uuid)
+        self.loop.create_task(self.init_notif())
+        # asyncio.run(self.init_notif())
 
     def cleanup(self):
-        print("cleaning up")
-        _ = self.disconnect()
+        self.loop.create_task(self.client.stop_notify(self.uuid))
+        super().cleanup()
 
-    @asyncClose
-    async def disconnect(self):
-        print("disconnecting")
-        await self.client.disconnect()
-        print("disconnected successfully")
+    def get_value(self):
+        try:
+            self.value.emit(float(np.frombuffer(self.current_val, dtype=np.int32)[0])/(2**16))
+        except ValueError:
+            self.value.emit(0)
 
-    def send_value(self, value: bytes):
-        self.loop.create_task(self.client.write_gatt_char(self.uuid, value))
+    async def init_notif(self):
+        await self.connect_task
+        await self.client.start_notify(self.uuid, lambda char, data: self.set_current_val(data))
         
 
 if __name__ == "__main__":
@@ -63,8 +87,11 @@ if __name__ == "__main__":
     asyncio.set_event_loop(event_loop)
     # 0409
     # EF68xxxx-9B35-4933-9B10-52FFA9740042
-    battery_graph = DataPlot(ReadThread("F1:EC:95:17:0A:62", "EF680409-9B35-4933-9B10-52FFA9740042"), y_min=0, y_max=100, datarate=1)
-    battery_graph.send_value(bytes.fromhex('0001'))
+    # battery_graph = DataPlot(ReadThread("F1:EC:95:17:0A:62", "00002a19-0000-1000-8000-00805f9b34fb"), y_min=0, y_max=100, datarate=1)
+    # battery_graph.show()
+    # app.aboutToQuit.connect(battery_graph.cleanup)
+
+    battery_graph = DataPlot(NotifyThread("F1:EC:95:17:0A:62", "EF680409-9B35-4933-9B10-52FFA9740042"), y_min=0, y_max=360, datarate=200, num_data_points=400)
     battery_graph.show()
     app.aboutToQuit.connect(battery_graph.cleanup)
     event_loop.run_forever()
