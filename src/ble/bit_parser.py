fbdfc9b7e80e6095ac54eaa9cb6635eeb2184552
griffdawg123 <doylegriffin32@gmail.com>
2024-03-13 14:05:52 +1100

    bit_parser working

--- src/ble/bit_parser.py
+++ src/ble/bit_parser.py
@@ -1,12 +1,56 @@
-from typing import List, Any, NewType, Union
+from typing import List, Any, NewType, Union, TypeVar
 import numpy as np
+from numpy.typing import NBitBase
+
+T2 = TypeVar("T2", bound=NBitBase)
+
+int_types = {
+    "int16" : np.int16,
+    "int32" : np.int32,
+    "uint16" : np.uint16,
+    "uint32" : np.uint32,
+}
 
 class BitParser():
     def __init__(self, config: dict[str, Any]) -> None:
         self.config = config
+        self.num_values = config["num_values"]
+        self.num_bytes = config["total_bytes"]
+        self.name = config["name"]
+        self.services = config["services"]
 
     def parse_bits(self, data: bytes):
-        pass
+        if len(data) > self.num_bytes:
+            raise ValueError("Incoming data too long")
+        elif len(data) < self.num_bytes:
+            raise ValueError("Incoming data too short")
+        
+        result = []
+        for s in self.services:
+            value = int.from_bytes(data[:s["num_bytes"]], 'big')
+            print(data[:s["num_bytes"]])
+            result.append(self.Q_decode(value, s["encoding"]))
+            data = data[s["num_bytes"]:]
+        return result
 
-    def parse_data(self, data: List[np.unsignedinteger | np.signedinteger]):
-        pass
+    def parse_data(self, data: List[int]):
+        if len(data) > self.num_values:
+            raise ValueError("Data to encode is too long")
+        elif len(data) < self.num_values:
+            raise ValueError("Data to encode is too short")
+        
+        result = b''
+        for s in self.services:
+            to_encode = self.Q_encode(data.pop(0), s["encoding"])
+            print(to_encode)
+            result += to_encode.to_bytes(s["num_bytes"], "big")
+        return result
+    
+    def Q_encode(self, num: float, encoding: str):
+        _, decimal = encoding.split("Q")
+        return (num*2**int(decimal))
+
+    def Q_decode(self, num: int, encoding: str):
+        _, decimal = encoding.split("Q")
+        return float(num/2**int(decimal))
+
--- testing/test_bit_parser.py
+++ testing/test_bit_parser.py
@@ -7,8 +7,8 @@ from src.ble.bit_parser import BitParser
 def parser_config() -> dict[str, Any]:
     return {
         "name" : "Raw Data",
-        "num_values" : 9,
-        "total_bytes" : 18,
+        "num_values" : 2,
+        "total_bytes" : 4,
         "services" : [
             {
                 "name" : "accel_x",
@@ -24,55 +24,6 @@ def parser_config() -> dict[str, Any]:
                 "encoding" : "6Q10",
                 "units" : "G"
             },
-            {
-                "name" : "accel_z",
-                "data_type" : "int16",
-                "num_bytes" : 2,
-                "encoding" : "6Q10",
-                "units" : "G"
-            },
-            {
-                "name" : "gyro_x",
-                "data_type" : "int16",
-                "num_bytes" : 2,
-                "encoding" : "11Q5",
-                "units" : "deg/s"
-            },
-            {
-                "name" : "gyro_y",
-                "data_type" : "int16",
-                "num_bytes" : 2,
-                "encoding" : "11Q5",
-                "units" : "deg/s"
-            },
-            {
-                "name" : "gyro_z",
-                "data_type" : "int16",
-                "num_bytes" : 2,
-                "encoding" : "11Q5",
-                "units" : "deg/s"
-            },
-            {
-                "name" : "comp_x",
-                "data_type" : "int16",
-                "num_bytes" : 2,
-                "encoding" : "12Q4",
-                "units" : "micT"
-            },
-            {
-                "name" : "comp_y",
-                "data_type" : "int16",
-                "num_bytes" : 2,
-                "encoding" : "12Q4",
-                "units" : "micT"
-            },
-            {
-                "name" : "comp_z",
-                "data_type" : "int16",
-                "num_bytes" : 2,
-                "encoding" : "12Q4",
-                "units" : "micT"
-            },
         ]
     }
 
@@ -80,14 +31,45 @@ def parser_config() -> dict[str, Any]:
 def bit_parser(parser_config):
     return BitParser(parser_config)
 
-def test_encode_bits_success():
-    pass
+def test_encode_bits_success(bit_parser):
+    # x = 10, y = 5
+    # x_Q = 10240, y_Q = 5120
+    # x_B = 0010100000000000, y_B = 0001010000000000
+    # x_H = \x28\x00, y_H = \x14\x00
+    # desired_result: bytes = b'00101000000000000001010000000000'
+    desired_result: bytes = b'\x28\x00\x14\x00'
+    assert bit_parser.parse_data([10, 5]) == desired_result
 
-def test_encode_bits_fail():
-    pass
+def test_encode_zero(bit_parser):
+    assert bit_parser.parse_data([0, 0]) == b'\x00\x00\x00\x00'
 
-def test_decode_bits_success():
-    pass
+def test_encode_bits_fail_too_many(bit_parser):
+    with pytest.raises(ValueError):
+        bit_parser.parse_data([1, 2, 3])
 
-def test_decode_bits_fail():
-    pass
+def test_encode_bits_fail_not_enough(bit_parser):
+    with pytest.raises(ValueError):
+        bit_parser.parse_data([1])
+
+def test_decode_bits_success(bit_parser):
+    desired_result = [10, 5]
+    assert bit_parser.parse_bits(b'\x28\x00\x14\x00') == desired_result
+
+def test_decode_zero(bit_parser):
+    assert bit_parser.parse_bits(b'\x00\x00\x00\x00') == [0, 0]
+
+def test_decode_bits_fail_too_long(bit_parser):
+    with pytest.raises(ValueError):
+        bit_parser.parse_bits(b'\x00\x01')
+
+def test_decode_bits_fail_too_short(bit_parser):
+    with pytest.raises(ValueError):
+        bit_parser.parse_bits(b'\x28\x00\x14\x00\x02\x04')
+
+def test_Q_encoding(bit_parser):
+    encoding = "6Q10"
+    assert bit_parser.Q_encode(10, encoding) == 10240
+
+def test_Q_decoding(bit_parser):
+    encoding = "6Q10"
+    assert bit_parser.Q_decode(10240, encoding) == 10
