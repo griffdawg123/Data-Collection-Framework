9c00bac8915547a55f63bcf91b3fdaeb764b6d1d
griffdawg123 <doylegriffin32@gmail.com>
2024-03-30 13:44:31 +1100

    ble status

--- src/widgets/ble_status.py
+++ src/widgets/ble_status.py
@@ -1,47 +1,76 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton
+import asyncio
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton
 import sys
+from qasync import QEventLoop
+from bleak import BleakClient
 
 from src.widgets.led_indicator import LEDColor, LEDIndicator
 
 class BLEStatus(QWidget):
-    def __init__(self, device_label: str) -> None:
+    def __init__(self, device_label: str, device_address: str) -> None:
         super().__init__()
         self.label = device_label
+        self.address = device_address
+        self.client = BleakClient(self.address)
+        self.title_label = QLabel(self.label)
+        self.current_status = LEDColor.IDLE
+        self.stat_led = LEDIndicator(self.current_status)
+        self.stat_label = QLabel(self.current_status.name)
+        self.retry = QPushButton()
         self.init_ui()
-        self.show()
+        self.init_ble()
 
     def init_ui(self):
-        self._layout = QVBoxLayout()
-        self.title_label = QLabel()
-        self.title_label.setText(self.label)
-        self.title_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self._layout.addWidget(self.title_label)
+        layout = QVBoxLayout()
+        layout.addWidget(self.title_label)
+
         self.indicator_widget = QWidget()
-        self.indicator_layout = QHBoxLayout()
+        indicator_layout = QHBoxLayout()
+        indicator_layout.addWidget(self.stat_led)
+        indicator_layout.addWidget(self.stat_label)
+        self.indicator_widget.setLayout(indicator_layout)
+        layout.addWidget(self.indicator_widget)
 
-        current_status = LEDColor.ERROR
+        self.retry.setText("Retry")
+        self.retry.clicked.connect(self.init_ble)
+        layout.addWidget(self.retry)
 
-        self.stat_led = LEDIndicator(current_status)
-        self.indicator_layout.addWidget(self.stat_led)
-
-        self.stat_label = QLabel(current_status.name)
-        self.indicator_layout.addWidget(self.stat_label)
-
-        self.indicator_widget.setLayout(self.indicator_layout)
-        self._layout.addWidget(self.indicator_widget)
-
-        self.connect = QPushButton
-
-        self.setLayout(self._layout)
+        self.setLayout(layout)
         self.setStyleSheet("border: 1px solid black;")
+
+    def init_ble(self):
+        self.current_status = LEDColor.IDLE
+        self._update()
+        event_loop = asyncio.get_event_loop()
+        connect_task = event_loop.create_task(self.client.connect())
+        connect_task.add_done_callback(self.set_status)
+
+    def set_status(self, task: asyncio.Task):
+        exp = task.exception()
+        if exp:
+            print(exp)
+            self.current_status = LEDColor.ERROR
+        elif self.client.is_connected:
+            print(f"Connected to device: {self.address}")
+            self.current_status = LEDColor.OKAY
+        else:
+            print("Retry")
+            self.current_status = LEDColor.IDLE
+        self._update()
+
+    def _update(self):
+        self.stat_label.setText(self.current_status.name)
+        self.stat_led.set_status(self.current_status)
+        self.update()
 
 
 
 if __name__=="__main__":
+
     app = QApplication(sys.argv)
-    mwindow = QWidget()
-    status = BLEStatus("THINGY")
-    mwindow.setGeometry(200, 200, 600, 600)
-    mwindow.show()
-    sys.exit(app.exec())
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+    status = BLEStatus("THINGY", "F1:EC:95:17:0A:62")
+    status.setGeometry(200, 200, 100, 100)
+    status.show()
+    loop.run_forever()
--- src/widgets/led_indicator.py
+++ src/widgets/led_indicator.py
@@ -18,9 +18,17 @@ class LEDIndicator(QLabel):
     def init_ui(self):
         self.setText("")
         self.resize(self.radius, self.radius)
-        self.setStyleSheet(f"border-radius : {self.radius // 2}; border : 1px solid black; background-color : {self.colour.value}")
+        self._update()
+
+    def set_status(self, status: LEDColor):
+        self.colour = status
+        self._update()
 
     def resizeEvent(self, event) -> None:
         self.resize(self.radius, self.radius)
+
+    def _update(self):
+        self.setStyleSheet(f"border-radius : {self.radius // 2}; border : 1px solid black; background-color : {self.colour.value}")
+        self.update()
 
     
