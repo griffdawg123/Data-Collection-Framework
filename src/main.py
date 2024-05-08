39f05feb0971168d914be00166656e75801c059c
griffdawg123 <doylegriffin32@gmail.com>
2024-05-08 13:57:00 +1000

    refactored main

--- src/main.py
+++ src/main.py
@@ -1,28 +1,24 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QApplication
 from src.windows.workspace import Workspace
-import logging
-import time
 from qasync import QEventLoop
 import asyncio
 
-DEBUG = True
-LOGGING = True
-
-log_name: str = "debug" if DEBUG else str(time.time())
-
-if __name__=="__main__":
+def main():
+    # Create application and workspace window
     app = QApplication(sys.argv)
-    event_loop = QEventLoop(app)
+    workspace = Workspace(app) 
 
+    # Extract event loop
+    event_loop = QEventLoop(app)
+    asyncio.set_event_loop(event_loop)
+
+    # Run app
     app_close_event = asyncio.Event()
     app.aboutToQuit.connect(app_close_event.set)
-    asyncio.set_event_loop(event_loop)
-    workspace = Workspace(app)
     with event_loop:
         event_loop.run_until_complete(app_close_event.wait())
 
-    # pending = asyncio.all_tasks(event_loop)
-    # loop = asyncio.new_event_loop()
-    # loop.run_until_complete(asyncio.gather(*pending))
+
+if __name__=="__main__":
+    main()
